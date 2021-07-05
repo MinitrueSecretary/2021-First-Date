eb1818689250e406e158e73ba6911f0881af6c23
Wipu <wipugeng@hotmail.com>
2021-07-05 14:20:35 +0700

    add notfound page

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-var info = document.getElementById("info");
-var button = document.getElementById("submit-button");
-button.onclick = function(){
-    console.log("Clicked!")
+var studentID;
+var phoneNumber;
+
+
+
+
+
+function indexFunction(){
+    var button = document.getElementById("submit-button");
+    
+
+    button.onclick = function(){
+        alert("hello");
+        window.location.href = "./notfound.html";
+    } 
+/*
+    button.onclick = function(){
+        studentID = document.getElementById("student-id").value;
+        phoneNumber = document.getElementById("phone-no").value;
+        console.log("Clicked!");
+        console.log("SID is " + studentID);
+        console.log("Phone no is " + phoneNumber);
+    }
+*/
+
 }
 
