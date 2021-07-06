db48a6849ce42d672bc710fe0420c5382b4cbcd8
Wipu <wipugeng@hotmail.com>
2021-07-06 21:14:08 +0700

    link backend stuffs to frontend

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyA_4Fr0-tEMG8Kkrn9xbyj4mueilp2sjf4",
+    authDomain: "firstdate2021-64031.firebaseapp.com",
+    projectId: "firstdate2021-64031",
+    storageBucket: "firstdate2021-64031.appspot.com",
+    messagingSenderId: "1087619777179",
+    appId: "1:1087619777179:web:9064e248e0eec6bf6efc3e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 var studentID;
 var phoneNumber;
-
-
-
-
 
 function indexFunction(){
     var button = document.getElementById("submit-button");
